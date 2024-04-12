263e123faf818b4c560dd90d2536275cb5831bb5
EscapedSpark <113490727+hzqlingmou@users.noreply.github.com>
2024-04-12 22:16:45 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -40,8 +40,8 @@ export default hopeTheme({
     avatar: "https://q1.qlogo.cn/g?b=qq&s=0&nk=3139373986",
     intro: "/",
     medias: {
-      Baidu: "https://mr.baidu.com/r/19wFkMt0Gt2?f=cp&u=4d151be3e49afc29",
-      BiliBili: "https://b23.tv/8sOJIMw",
+      /3Baidu: "https://mr.baidu.com/r/19wFkMt0Gt2?f=cp&u=4d151be3e49afc29",
+      //BiliBili: "https://b23.tv/8sOJIMw",
 //      Discord: "https://discord.gg/VxqGvm5FKE",
 //      Email: "http://2833598659@qq.com",
       GitHub: "https://github.com/hzalingmou",
