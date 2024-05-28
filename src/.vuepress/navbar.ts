63acecf77aa80765a4d7a4e440c95e6591b3a165
escapedspark <167544161+escapedspark@users.noreply.github.com>
2024-05-28 22:08:42 +0800

    Update navbar.ts

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,7 +2,11 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  
+  {
+    text: "主页/Home",
+    icon: "lightlub",
+    link: "/",
+  },
   {
     text: "博客/Blog",
     icon: "book",
