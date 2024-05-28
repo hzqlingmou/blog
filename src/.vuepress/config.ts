b660f9ff8f398fca9bd731ac5cf8177ad39c9ae5
escapedspark <167544161+escapedspark@users.noreply.github.com>
2024-05-28 22:51:24 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,13 +1,108 @@
 import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
-  base: "/",
 
   lang: "zh-CN",
-  title: "EscapedSpark-Share",
-  description: "逸燧の涩图站",
+  title: "Esca-Plugin文档",
+  description: "「我思故我在,一念定乾坤」",
 
-  theme,
+  theme: hopeTheme({
+  hostname: "https://mister-hope.github.io",
 
+  author: {
+    name: "逸燧EscapedSpark",
+    url: "https://escaped.icu",
+  },
+
+  iconAssets: "fontawesome-with-brands",
+
+  logo: "https://pi.escaped.icu/1.png",
+
+  repo: "hzqlingmou/esca-plugin",
+
+  docsDir: "src",
+
+  navbar: [
+  {
+    text: "主页/Home",
+    icon: "lightlub",
+    link: "/",
+  },
+  {
+    text: "博客/Blog",
+    icon: "book",
+    link: "https://escaped.icu",
+  },
+  ],
+  sidebar: [
+  {
+      text: "Pixiv涩图",
+      icon: "book",
+      prefix: "/pixiv/",
+      children: "structure",
+  },
+  ],
+
+  // 页脚
+  footer: "「梦在前方,路在脚下」",
+  displayFooter: true,
+
+  // 博客相关
+  blog: {
+    description: "不要让你的意志击穿了你的欲望",
+    intro: "https://escaped.icu",
+    medias: {
+      QQ: "https://qm.qq.com/q/JDf3REVSMM",
+      Gitee: "https://gitee.com/escaped-spark",
+      GitHub: "https://github.com/hzqlingmou",
+    },
+  },
+  
+  metaLocales: {
+    editLink: "温馨❤️提示:要想身体好，同志不要导",
+  },
+  
+  plugins: {
+    blog: true,
+     comment: {
+        provider: "Waline",
+        serverURL: "https://waline.dengfenglai.icu/",
+      },
+
+    components: {
+      components: ["Badge", "VPCard"],
+    },
+    mdEnhance: {
+      alert: true,
+      align: true,
+      attrs: true,
+      codetabs: true,
+      component: true,
+      demo: true,
+      figure: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      mark: true,
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+    },
+  },
+}),
 });
